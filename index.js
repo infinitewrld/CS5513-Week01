64e2ca4d60c9a683be31035e87c99355d13004d1
khalil bhangoo <9030533-iloveyouritual@users.noreply.replit.com>
2024-09-10 22:30:00 +0000

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-// Use this workspace to submit your homework for Week 1: Assignment 2: Git + GitHub Basics
+
+console.log("hello world")
+//this workspace to submit your homework for Week 1: Assignment 2: Git + GitHub Basics
